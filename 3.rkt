54482a0c7c5bb0ea45890f8c4ee6e02cc6a492d5
doyougnu <jmy6342@gmail.com>
2020-09-08 21:43:12 -0700

    3

--- 3.rkt
+++ 3.rkt
@@ -13,9 +13,120 @@
       (fresh (d)
              (cdro l d)
              (listo d))]
+     [succeed fail]))) ;; else fail
+
+(define lolo
+;; list of lists - o
+  (λ (l)
+    (conde
+     [(nullo l) succeed]
+     [(fresh (a)
+             (caro l a)
+             (listo a))
+      (fresh (d)
+             (cdro l d)
+             (lolo d))]
      [succeed fail])))
 
+;; 31
+;; (define twinso
+;;   (λ (s)
+;;     (fresh (x y)
+;;            (conso x y s)
+;;            (conso x '() y))))
 
+(define twinso
+  (λ (y)
+    (fresh (x)
+           (== `(,x ,x) y))))
+
+;; (define loto
+;;   (λ (l)
+;;     (conde
+;;      [(nullo l) succeed]
+;;      [(fresh (a)
+;;              (caro l a)
+;;              (twinso a))
+;;       (fresh (d)
+;;              (cdro l d)
+;;              (loto d))]
+;;      [succeed fail])))
+(define loto
+  (λ (l)
+    (listofo twinso l)))
+
+(define listofo
+  (λ (predo l)
+    (conde
+     [(nullo l) succeed]
+     [(fresh (a)
+             (caro l a)
+             (predo a))
+      (fresh (d)
+             (cdro l d)
+             (listofo predo d))]
+     [succeed fail])))
+
+(define eq-caro
+  (λ (l x)
+    (caro l x)))
+
+(define membero
+  (λ (x l)
+    (conde
+     ;; [(nullo l) fail]
+     [(eq-caro l x) succeed]
+     [succeed (fresh (d)
+                     (cdro l d)
+                     (membero x d))])))
+
+(define identity
+  (λ (l)
+    (run* (y)
+          (membero y l))))
+
+(define pmembero
+  (λ (x l)
+    (conde
+     [(eq-caro l x) (fresh (a d)
+                           (cdro l `(,a . ,d)))]
+     [(eq-caro l x) (cdro l '())]
+     [succeed (fresh (d)
+                     (cdro l d)
+                     (pmembero x d))])))
+
+(define first-value
+  (λ (l)
+    (run 1 (y)
+         (membero y l))))
+
+(define memberrevo
+  (λ (x l)
+    (conde
+     [(nullo l ) fail]
+     [succeed (fresh (d)
+                     (cdro l d)
+                     (memberrevo x d))]
+     [succeed
+      (eq-caro l x)])))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; tests ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (check 7 (run* (x) (listo `('a 'b ,x 'd))) '(_.0))
 (check 10 (run 1 (x) (listo `('a 'b 'c . ,x))) '(()))
 (explicate 14 (run 5 (x) (listo `('a 'b 'c . ,x))) "we are asking for 5 results")
+(check 20 (run 1 (l) (lolo l)) '(()))
+(check 32 (run* (q) (twinso `('tofu 'tofu)) (== #t q)) '(#t))
+(check 32 (run* (q) (twinso `(,q 'tofu))) '('tofu))
+(check 38 (run 1 (z) (loto `('('g 'g) . ,z))) '())
+(explicate 42 (run 5 (z) (loto `(('g 'g) . ,z))) "we asked for 5 results from loto")
+(explicate 47 (run 3 (out) (fresh (w x y z)
+                                  (== `((g g) (e ,w) (,x ,y) . ,z) out)
+                                  (loto out))) "we are putting it through its paces")
+(explicate 49 (run 3 (out) (fresh (w x y z)
+                                  (== `((g g) (e ,w) (,x ,y) . ,z) out)
+                                  (listofo twinso out)))
+           "we are testing out listofo")
+(check 54 (run 1 (y) (membero y `('hummus 'with 'pita))) '('hummus))
+(explicate 76 (run 5 (l) (membero 'tofu l)) "we building lists bby")
+(explicate 80 (run 5 (l) (pmembero 'tofu l)) "we building lists smart bby")
+(explicate 89 (run 12 (l) (pmembero 'tofu l)) "we building lists excessively bby")
--- utils.rkt
+++ utils.rkt
@@ -10,21 +10,21 @@
        #'(module+ test
            (let* ([test-passed (equal? question answer)]
                   [result-message (if test-passed "Passed" "Failed")])
-             (printf  "~a   ===>   ~s~n" id result-message))))]
+             (printf  "~a   ===>   ~s~n~n" id result-message))))]
     [(_ id question answer message)
      (with-syntax ([id (~r (syntax->datum #'id) #:min-width 2 #:pad-string " ")])
        #'(module+ test
            (let* ([test-passed (equal? question answer)]
                   [result-message (if test-passed "Passed" "Failed")])
-             (printf  "~a ===> ~s with: ~s~n" id message))))]))
+             (printf  "~a ===> ~s with: ~s~n~n" id message))))]))
 
 (define-syntax (explicate stx)
   (syntax-case stx ()
     [(_ id question)
      (with-syntax ([id (~r (syntax->datum #'id) #:min-width 2 #:pad-string " ")])
        #'(module+ test
-           (printf  "~a becomes ~s~n" id question)))]
+           (printf  "~a becomes ~s~n~n" id question)))]
     [(_ id question message)
      (with-syntax ([id (~r (syntax->datum #'id) #:min-width 2 #:pad-string " ")])
        #'(module+ test
-           (printf  "~a becomes ~s because ~s~n" id question message)))]))
+           (printf  "~a becomes ~s because ~s~n~n" id question message)))]))
